38104fa1c9b06c1a789bc0bb382fcbf9ed3a1104
Emre Hasegeli <hasegeli@tart.com.tr>
2014-01-11 15:44:08 +0200

    Format emails before sending.

--- db/005-emailToSend.sql
+++ db/005-emailToSend.sql
@@ -1,13 +1,20 @@
 Begin;
 
-Create or replace function EmailToSendCount ()
+Create or replace function EmailToSendCount()
     returns bigint
     language sql
     as $$
 Select count(*) from EmailSend where status = 'waiting'
 $$;
 
-Create or replace function NextEmailToSend ()
+Create or replace function FormatEmailToSend(body text, k text[], v text[])
+    returns text
+    language plpythonu
+    as $$
+return body.format(**dict(zip(k, v)))
+$$;
+
+Create or replace function NextEmailToSend()
     returns table (
         toName varchar(200),
         toAddress varchar(200),
@@ -33,12 +40,22 @@ With FirstWaitingEmail as (select * from EmailSend
             Subscriber.emailAddress as toAddress,
             Email.fromName,
             Email.fromAddress,
-            Email.subject,
-            Email.plainBody,
-            Email.hTMLBody
+            FormatEmailToSend(Email.subject, Formatter.k, Formatter.v) as subject,
+            FormatEmailToSend(Email.plainBody, Formatter.k, Formatter.v) as plainBody,
+            FormatEmailToSend(Email.hTMLBody, Formatter.k, Formatter.v) as hTMLBody
         from UpdatedEmailSend
             join Subscriber on UpdatedEmailSend.subscriberId = Subscriber.id
             join Email on UpdatedEmailSend.emailId = Email.id
+                cross join lateral (select array_agg(key) as k, array_agg(value) as v
+                    from ((select * from each(Subscriber.properties))
+                        union values ('fullname', Subscriber.fullName),
+                                ('unsubscribeurl',
+                                    Email.returnURLRoot || 'unsubscribe/' || EmailHash(UpdatedEmailSend)),
+                                ('redirecturl',
+                                    Email.returnURLRoot || 'subscriber/' || EmailHash(UpdatedEmailSend)),
+                                ('trackerimageurl',
+                                    Email.returnURLRoot || 'trackerImage/' ||
+                                                EmailHash(UpdatedEmailSend))) as A) as Formatter
 $$;
 
 Commit;
