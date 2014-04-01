5e172b19be45cd33b0dd21251adf2b4530a0710f
Emre Hasegeli <hasegeli@tart.com.tr>
2014-04-01 12:57:32 +0300

    Split response report part on worker/receive.py.

--- worker/receive.py
+++ worker/receive.py
@@ -66,86 +66,95 @@ def main():
     for messageId in messageIds[:amount]:
         message = email.message_from_string(server.execute('fetch', messageId, '(RFC822)')[0][1], Message)
         message.check()
-        report = {}
-        emailSend = None
+        returnedOriginal = None
 
         if message.get_content_type() in ('multipart/report', 'multipart/mixed'):
             # The last payload must be the returned original.
             returnedOriginal = message.get_payload()[-1]
         elif message.get_content_type() in ('text/plain', 'multipart/alternative'):
+            warning('Unexpected plain text email message will be processed as returned original:', message)
             returnedOriginal = message
-            warning('Unexpected plain text email message will be processed:', report)
         else:
             warning('Unexpected MIME type:', message)
 
-        if returnedOriginal.is_multipart():
-            report['originalHeaders'] = dict(returnedOriginal.get_payload(0).headers())
-        else:
-            splitMessage = returnedOriginal.splitSubmessage()
-            if splitMessage:
-                report['body'], submessage = splitMessage
-                report['originalHeaders'] = dict(submessage.headers())
+        if returnedOriginal:
+            report = {}
+
+            if returnedOriginal.is_multipart():
+                report['originalHeaders'] = dict(returnedOriginal.get_payload(0).headers())
             else:
-                report['body'] = returnedOriginal.plainestWithoutQuote()
+                splitMessage = returnedOriginal.splitSubmessage()
+                if splitMessage:
+                    report['body'], submessage = splitMessage
+                    report['originalHeaders'] = dict(submessage.headers())
+                else:
+                    report['body'] = returnedOriginal.plainestWithoutQuote()
 
-        if message.get_content_type() in ('multipart/report', 'multipart/mixed'):
-            # Aditional fields for the standart response reports.
-            if len(message.get_payload()) > 1:
-                report['body'] = message.get_payload(0).plainest()
-                if len(message.get_payload()) > 2:
-                    report['fields'] = dict(message.get_payload()[-2].recursiveHeaders())
+            if message.get_content_type() in ('multipart/report', 'multipart/mixed'):
+                # Aditional fields for the standart response reports.
+                if len(message.get_payload()) > 1:
+                    report['body'] = message.get_payload(0).plainest()
+                    if len(message.get_payload()) > 2:
+                        report['fields'] = dict(message.get_payload()[-2].recursiveHeaders())
 
-        if 'originalHeaders' in report and 'list-unsubscribe' in report['originalHeaders']:
-            unsubscribeURL = report['originalHeaders']['list-unsubscribe'][1:-1]
-            with postgres:
-                emailSend = postgres.call('EmailSendFromUnsubscribeURL',
-                                          [sender, unsubscribeURL] if sender else unsubscribeURL)
-
-        else:
-            emailAddresses = []
-
-            if 'fields' in report:
-                def addressInHeader(value):
-                    return value.split(';')[1] if ';' in value else value
-
-                if 'original-recipient' in report['fields']:
-                    emailAddresses.append(addressInHeader(report['fields']['original-recipient']).lower())
-
-                if 'original-rcpt-to' in report['fields']:
-                    emailAddresses.append(report['fields']['original-rcpt-to'].lower())
-
-                if 'final-recipient' in report['fields']:
-                    emailAddresses.append(addressInHeader(report['fields']['final-recipient']).lower())
-
-            if 'originalHeaders' in report and 'to' in report['originalHeaders']:
-                emailAddresses.append(reports['originalHeaders']['to'].lower())
-
-            if not emailAddresses and 'body' in report:
-                emailAddresses = [e.lower() for e in re.findall('[A-Za-z0-9._\-+!'']+@[A-Za-z0-9.\-]+\.[A-Za-z0-9]+',
-                                                                report['body'])]
-
-            if emailAddresses:
-                with postgres:
-                    try:
-                        emailSend = postgres.call('LastEmailSendToEmailAddresses',
-                                                  [sender, emailAddresses] if sender else emailAddresses)
-                    except PostgresNoRow: pass
-
-        if emailSend:
-            report['emailId'] = emailSend['emailid']
-            report['subscriberId'] = emailSend['subscriberid']
-
-            with postgres:
-                postgres.insert('EmailSendResponseReport', report)
+            if addResponseReport(sender, report):
                 print(messageId + '. email message processed and will be deleted.')
                 server.execute('store', messageId, '+FLAGS', '\Deleted')
-        else:
-            warning('Email could not found in the database:', message)
+            else:
+                warning('Email could not found in the database:', message)
 
     if debug:
         print('Deleted emails will be left on the server in debug mode.')
     else:
         server.execute('expunge')
+
+def addResponseReport(sender, report):
+    emailSend = None
+
+    if 'originalHeaders' in report and 'list-unsubscribe' in report['originalHeaders']:
+        unsubscribeURL = report['originalHeaders']['list-unsubscribe'][1:-1]
+        with postgres:
+            emailSend = postgres.call('EmailSendFromUnsubscribeURL', ([sender] or []) + [unsubscribeURL])
+
+    else:
+        addresses = []
+
+        if 'fields' in report:
+            def addressInHeader(value):
+                return value.split(';')[1] if ';' in value else value
+
+            if 'original-recipient' in report['fields']:
+                addresses.append(addressInHeader(report['fields']['original-recipient']).lower())
+
+            if 'original-rcpt-to' in report['fields']:
+                addresses.append(report['fields']['original-rcpt-to'].lower())
+
+            if 'final-recipient' in report['fields']:
+                addresses.append(addressInHeader(report['fields']['final-recipient']).lower())
+
+        if 'originalHeaders' in report and 'to' in report['originalHeaders']:
+            addresses.append(reports['originalHeaders']['to'].lower())
+
+        if not addresses and 'body' in report:
+            addresses = [e.lower() for e in re.findall('[A-Za-z0-9._\-+!'']+@[A-Za-z0-9.\-]+\.[A-Za-z0-9]+',
+                                                       report['body'])]
+
+        if addresses:
+            with postgres:
+                try:
+                    emailSend = postgres.call('LastEmailSendToEmailAddresses', ([sender] or []) + [addresses])
+                except PostgresNoRow: pass
+
+    if emailSend:
+        report['fromAddress'] = emailSend['fromaddress']
+        report['toAddress'] = emailSend['toaddress']
+        report['emailId'] = emailSend['emailid']
+
+        with postgres:
+            postgres.insert('EmailSendResponseReport', report)
+            return True
+
+    return False
 
 if __name__ == '__main__':
     main()
