e8d444f94bbe6d435981c9f2e2ebd0d3b8d2b2ac
Emre Hasegeli <hasegeli@tart.com.tr>
2014-04-19 14:39:28 +0300

    Do not use variationId in EmailHash.

--- db/003-emailHash.sql
+++ db/003-emailHash.sql
@@ -16,8 +16,7 @@ Create or replace function EmailHash(EmailSend)
 Select Base64ForURL(digest('secret1' || $1.fromAddress ||
                            'secret2' || $1.toAddress ||
                            'secret3' || to_hex($1.emailId::integer) ||
-                           'secret4' || to_hex($1.variationId::integer) ||
-                           'secret5', 'sha256'))
+                           'secret4', 'sha256'))
 $$;
 
 Create unique index EmailSendEmailHashI on EmailSend (EmailHash(EmailSend));
